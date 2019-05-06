ad3b4bb39231f5ae638102115eb7419705d33146
Pozitronik <antikillerxxl@gmail.com>
2019-05-06 14:57:03 +0300

    Show action added

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -3,9 +3,13 @@ declare(strict_types = 1);
 
 namespace pozitronik\arlogger\controllers;
 
+use pozitronik\arlogger\models\ActiveRecordLogger;
 use pozitronik\arlogger\models\ActiveRecordLoggerSearch;
+use ReflectionException;
 use Throwable;
 use Yii;
+use yii\base\InvalidConfigException;
+use yii\base\UnknownClassException;
 use yii\web\Controller;
 
 /**
@@ -21,13 +25,30 @@ class DefaultController extends Controller {
 	public function actionIndex():string {
 		$params = Yii::$app->request->queryParams;
 		$searchModel = new ActiveRecordLoggerSearch();
-
-
 		return $this->render('index', [
 			'searchModel' => $searchModel,
 			'dataProvider' => $searchModel->search($params)
 		]);
+	}
 
+
+	/**
+	 * @param string $for
+	 * @param int $id
+	 * @return string
+	 * @throws ReflectionException
+	 * @throws Throwable
+	 * @throws InvalidConfigException
+	 * @throws UnknownClassException
+	 */
+	public function actionShow(string $for, int $id):string {
+		$logger = new ActiveRecordLogger([
+			'model' => $for
+		]);
+
+		return $this->render('timeline', [
+			'timeline' => $logger->getHistory($id)
+		]);
 	}
 
 }
