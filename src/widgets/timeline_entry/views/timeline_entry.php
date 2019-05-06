b6c6be1c2bef97c59311ac9260393824a5c3b786
Pozitronik <antikillerxxl@gmail.com>
2019-05-06 14:58:11 +0300

    Icon replaced to user

--- src/widgets/timeline_entry/views/timeline_entry.php
+++ src/widgets/timeline_entry/views/timeline_entry.php
@@ -13,7 +13,7 @@ use yii\web\View;
 
 <div class="timeline-entry">
 	<div class="timeline-stat">
-		<div class="timeline-icon"><?= $entry->icon ?></div>
+		<div class="timeline-icon"><?= $entry->user ?></div>
 		<div class="timeline-time"><?= $entry->time ?></div>
 	</div>
 	<div class="timeline-label">
