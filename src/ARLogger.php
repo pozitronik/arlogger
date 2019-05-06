bb56b398470e84103a266cdea0ebc8c73ea0107f
Pozitronik <antikillerxxl@gmail.com>
2019-05-06 10:58:44 +0300

    Extension module stub

---
+++ src/ARLogger.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+namespace pozitronik\arlogger;
+
+
+use yii\gii\Module;
+
+/**
+ * Class ARLogger
+ * @package pozitronik\arlogger
+ */
+class ARLogger extends Module {
+}
